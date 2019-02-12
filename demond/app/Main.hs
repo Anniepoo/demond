36c065f8608ab0c9a1cd7bc9b89ca05e241c6047
Anne Ogborn <annie66us@yahoo.com>
2019-02-12 08:52:53 +0530

    minimal working

--- demond/app/Main.hs
+++ demond/app/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NumericUnderscores #-}
+
 module Main where
 
 import Lib
@@ -6,14 +8,30 @@ import Lib
 import Web.Spock
 import Web.Spock.Config
 
+import Web.Spock.Lucid (lucid)
+import Lucid
+
+import Control.Concurrent (forkIO, threadDelay)
+
 -- runSpock :: Port -> IO Middleware -> IO ()
 
 main :: IO ()
 main = do
   cfg <- defaultSpockCfg () PCNoDatabase ()
+  forkIO (do putStrLn "forked thread"
+             threadDelay 2_000_000
+             putStrLn "end thread")
   runSpock 8080 (spock cfg app)
 
 type Server a = SpockM () () () a
 
 app :: Server ()
-app = get root (text "Hello!")
+app = do get root $ lucid $ do
+                            h1_ "Hello!"
+                            p_ "How are you today?"
+         get "foo" $ lucid $ do
+                             h1_ "FOO!"
+
+-- Control.Concurrent  Concurrent and Parallel programming in Haskell
+
+-- https://haskell-at-work.com/episodes/2018-04-09-your-first-web-application-with-spock.html
