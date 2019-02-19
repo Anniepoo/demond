152a6fa2135b450f15577df8928f2b767f5e8840
Anne Ogborn <annie66us@yahoo.com>
2019-02-19 13:57:00 +0530

    graphql example working

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import NotEnoughPlayers from './notenoughplayers.js';
 import GameWorld from './gameworld.js';
 import ChatOverlay, { ChatBar } from './chat.js';
 import NeedArea from './needarea.js';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "https://rakshasa-game.herokuapp.com/v1alpha1/graphql"
+});
 
 class App extends Component {
     render() {
-      return (
+        return (
+    <ApolloProvider client={client}>
       <div id="app">
         <div id="gamelayer">
           <div id="gamearea">
@@ -18,11 +27,44 @@ class App extends Component {
           <NeedArea />
           <ChatBar />
         </div>
-      <NotEnoughPlayers />
-      <NameEntry />
-    </div>
+        <NotEnoughPlayers />
+        <NameEntry />
+      </div>
+      <TestApollo />
+   </ApolloProvider>
     );
   }
 }
+
+const TestApollo = () => (
+  <Query
+    query={gql`
+      {
+        towers {
+          id
+          col
+          row
+        }
+      }
+    `}
+>
+   {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error 8c/</p>;
+console.log(data);
+
+      return (
+<div id="alltowers">
+{ data.towers.map(({ id, row, col }) => (
+        <div key={id}>
+          <p>{row}, {col}</p>
+        </div>
+      ))
+}
+</div>
+)
+    }}
+  </Query>
+);
 
 export default App;
--- src/nameentry.js
+++ src/nameentry.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 class NameEntry extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {name: '', classhide: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+      this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+      console.log('A name was submitted: ' + this.state.name);
+      this.setState({classhide: 'dismissed'});
+    event.preventDefault();
+  }
+
   render() {
       return (
-              <div id="nameentry">
-                <p>Name Entry</p>
-              </div>
+      <div id="nameentry" className={this.state.classhide}
+           onSubmit={this.handleSubmit}>
+        <div className="dialogbox">
+          <form name="getname">
+              <label htmlFor="name">Player Name</label>
+              <input type="text" name="name" value={this.state.name}
+                   placeholder="Name"
+                   onChange={this.handleChange}/>
+              <input type="submit" name="join" value="Join!" />
+          </form>
+        </div>
+      </div>
     );
   }
 }
