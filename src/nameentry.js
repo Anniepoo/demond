66a63ef5c2583e693c447436e4d3ac919da6a16f
Anne Ogborn <annie66us@yahoo.com>
2019-02-19 16:01:08 +0530

    in some broken hell

--- src/nameentry.js
+++ src/nameentry.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
+
+const ADD_NAME = gql`
+mutation addOnePlayer($p: players_insert_input!) {
+  insert_players(
+    objects: [$p]
+  ) {
+    returning {
+      id
+    }
+  }
+}
+`;
 
 class NameEntry extends Component {
   constructor(props) {
@@ -13,28 +28,40 @@ class NameEntry extends Component {
       this.setState({name: event.target.value});
   }
 
-  handleSubmit(event) {
-      console.log('A name was submitted: ' + this.state.name);
-      this.setState({classhide: 'dismissed'});
-    event.preventDefault();
-  }
-
   render() {
-      return (
-      <div id="nameentry" className={this.state.classhide}
-           onSubmit={this.handleSubmit}>
-        <div className="dialogbox">
-          <form name="getname">
-              <label htmlFor="name">Player Name</label>
-              <input type="text" name="name" value={this.state.name}
-                   placeholder="Name"
-                   onChange={this.handleChange}/>
-              <input type="submit" name="join" value="Join!" />
-          </form>
-        </div>
-      </div>
-    );
-  }
+    {({ loading, error, data }) => {
+         if (loading){ return <p>Loading...</p>};
+         if (error) return <p>Error 8c/</p>;
+         console.log(data);
+
+        const handleSubmit = (e) => {
+            console.log('A name was submitted: ' + this.state.name);
+            this.setState({classhide: 'dismissed'});
+            e.preventDefault();
+            mutate();
+        }
+
+        return (
+                <Mutation mutation={ADD_NAME}
+                          variables={{p: { name: this.state.name}} >
+
+                    <div id="nameentry" className={this.state.classhide}
+                        onSubmit={this.handleSubmit}>
+                        <div className="dialogbox">
+                             <form name="getname">
+                                 <label htmlFor="name">Player Name</label>
+                                 <input type="text" name="name" 
+                                        value={this.state.name}
+                                        placeholder="Name"
+                                        onChange={this.handleChange}/>
+                                 <input type="submit" name="join" 
+                                        value="Join!" />
+                             </form>
+                        </div>
+                    </div>
+                </Mutation>
+          );
+     }
 }
 
 export default NameEntry;
